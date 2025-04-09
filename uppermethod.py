ee227147b804a020e48b18f30a6f5d46155e7089
Adfuror <adfuror@gmail.com>
2025-04-09 10:20:13 +0530

    string functions

---
+++ uppermethod.py
@@ -0,0 +1,7 @@
+course = 'python for beginners'
+print(course.upper())
+print(course.lower())
+print(course.find("o"))
+print(course.find("beginners"))
+print(course.replace('beginners',"absolute beginners"))
+print("python" in course)
