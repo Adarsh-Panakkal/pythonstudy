9cc593238fd380cd325c34f7a26337ccbcfcb5a8
Adfuror <adfuror@gmail.com>
2025-04-10 08:26:30 +0530

    00

---
+++ forloop.py
@@ -0,0 +1,14 @@
+for item in "Python":
+    print(item)
+for j in ['MASH','arin','aravibd']:
+    print(j)
+for h in range(10):
+    print(h)
+for k in range(4,25,5):
+    print(k)
+
+prices = [10,20,35,25]
+total = 0
+for price in prices:
+    total += price
+print(f"Total:{total}")
