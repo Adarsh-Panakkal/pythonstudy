d7b3f0496a431c3f59fb127ce66adc6074566b3c
Adfuror <adfuror@gmail.com>
2025-04-13 18:03:08 +0530

    00

---
+++ keywordargument.py
@@ -0,0 +1,4 @@
+def greatworker(first_name , last_name):
+    print(f"Is your full name is {first_name} {last_name}")
+    print("Then you are passed the exam")
+greatworker("Adarsh", last_name="Panakkal")
